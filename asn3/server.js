03a3d243df073d5d99187054a3d9caca4f33734c
yograj-vijay-yadav <yograjyadav8888@gmail.com>
2026-05-08 21:04:06 +0530

    Fixed list display isuu

--- asn3/server.js
+++ asn3/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 // Serve static files from "public" folder
--- asn3b/server.js
+++ asn3b/server.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+import express from 'express';
+import mongoose from 'mongoose';
+
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 mongoose.connect('mongodb://127.0.0.1:27017/userdb');
 
